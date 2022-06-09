c28bb062d237aa8f25701d36f96b2031a5a746ca
Eliane Cristina <89078266+ElianeO@users.noreply.github.com>
2022-06-09 13:01:24 -0300

    Delete busca-sequencial.js

--- busca-sequencial.js
+++
@@ -1,8 +0,0 @@
-function buscaSequencial(vetor, valorBusca){
-    for(let i=0;i < vetor.length; i++){
-        if(vetor[i] === valorBusca) return i //encontrou valorBusca
-    }
-    return -1 // valorBusca mão existe
-}
-const nomes = require('./data/vetor-nomes')
-console.log('Posição de JULIO', buscaSequencial(nomes, 'JULIO'))
